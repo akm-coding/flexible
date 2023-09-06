e35c289e13376c91be75f2a28f2627c18c751fd7
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2023-09-06 21:33:03 +0630

    Develop the Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,84 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
+
 export default function Footer() {
-  return <div></div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Flexible"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Flexible is the world's leading community for creatives to share,
+            and get hired.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[1].title}
+              links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              links={footerLinks[2].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[3].title}
+            links={footerLinks[3].links}
+          />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[4].title}
+              links={footerLinks[4].links}
+            />
+            <FooterColumn
+              title={footerLinks[5].title}
+              links={footerLinks[5].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[6].title}
+            links={footerLinks[6].links}
+          />
+        </div>
+      </div>
+      <div className="flexBetween footer_copyright">
+        <p>@ 2023 Flexibble. All rights reserved</p>
+        <p className="text-gray">
+          Developed by{" "}
+          <span className="text-black font-semibold">Aung Kaung Myat</span>
+        </p>
+      </div>
+    </footer>
+  );
 }
