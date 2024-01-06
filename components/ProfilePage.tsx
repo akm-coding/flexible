7227e29954e3c99728808397d9047b26891f7c57
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-01-07 01:16:07 +0630

    [Modify] Profile page

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -21,7 +21,7 @@ const ProfilePage = ({ user }: Props) => (
         />
         <p className="text-4xl font-bold mt-10">{user?.name}</p>
         <p className="md:text-5xl text-3xl font-extrabold md:mt-10 mt-5 max-w-lg">
-          I’m Software Engineer at ACE Data Systems Ltd. 👋
+          I’m Software Engineer at a Software Company 👋
         </p>
 
         <div className="flex mt-8 gap-5 w-full flex-wrap">
